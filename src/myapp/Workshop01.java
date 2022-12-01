5fcc613aebc4ca0be871c21fb771ba675599e451
wee_seng <wee.bscl@gmail.com>
2022-12-01 23:06:10 +0800

    initial commit

---
+++ src/myapp/Workshop01.java
@@ -0,0 +1,47 @@
+package myapp;
+
+import java.util.List;
+import java.io.Console;
+import java.util.LinkedList;
+
+public class Workshop01 {
+    public static void main(String[] args) {
+        // .split()
+        // .trim()
+        List<String> cart = new LinkedList<>();
+        Console cons = System.console();
+        while (true) {
+            String input = cons.readLine();
+            input = input.trim().toLowerCase();
+            // LIST
+            if (input.equals("list")) {
+                if (cart.size() == 0) {
+                    System.out.println("Your cart is empty");
+                } else {
+                    for (int i = 0; i < cart.size(); i++) {
+                        System.out.printf("%d. %s\n", (i + 1), cart.get(i));
+                    }
+                }
+                // DELETE
+            } else if (input.startsWith("delete")) {
+                Integer indexDel = (Integer.parseInt(input.split(" ")[1]));
+                if (indexDel > cart.size()) {
+                    System.err.println("Incorrect item index");
+                } else {
+                    System.out.printf("%s removed from cart\n", cart.get(indexDel - 1));
+                    // cart.remove(indexDel);
+                    cart.remove(indexDel - 1);
+                }
+                // ADD
+            } else if (input.startsWith("add")) {
+                String itemAdd = input.split(" ")[1];
+                if (cart.contains(itemAdd)) {
+                    System.err.printf("You have %s in your cart\n", itemAdd);
+                } else {
+                    cart.add(itemAdd);
+                    System.out.printf("%s added to cart\n", itemAdd);
+                }
+            }
+        }
+    }
+}
